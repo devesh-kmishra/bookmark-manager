5f6ae4a1d82295d2143351e97a4cf8fd071ef395
Devesh Krishna Mishra <mdevesh47@gmail.com>
2026-02-17 07:09:10 +0000

    update sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
-'use client'
+"use client";
 
-import { createClient } from "@/lib/supabase-client"
-import { FaGoogle } from "react-icons/fa"
+import { createClient } from "@/lib/supabase-client";
+import { FaGoogle } from "react-icons/fa";
 
 export default function Home() {
-  const supabase = createClient()
+  const supabase = createClient();
 
   const handleLogin = async () => {
     await supabase.auth.signInWithOAuth({
-      provider: 'google',
+      provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`
-      }
-    })
-  }
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+  };
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <button onClick={handleLogin} className="p-2 border-2 border-black rounded-lg hover:bg-blue-500 hover:text-white hover:border-blue-400">
+      <button
+        onClick={handleLogin}
+        className="p-2 border-2 border-black rounded-lg cursor-pointer hover:bg-blue-500 hover:text-white hover:border-blue-400"
+      >
         <FaGoogle className="inline-block mr-2" size={24} />
         Sign in with Google
       </button>
     </div>
-  )
+  );
 }
